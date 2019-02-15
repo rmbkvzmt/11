665209ff62caf983c0f93139ae63b4c82eca85d4
rmbkvzmt <azamatorymbekov@gmail.com>
2019-02-16 02:17:24 +0600

    comment for task3

--- Week2/Task3/Program.cs
+++ Week2/Task3/Program.cs
@@ -11,21 +11,21 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            DirectoryInfo dirIn = new DirectoryInfo(@"D:\Test");
+            DirectoryInfo dirIn = new DirectoryInfo(@"D:\Test");//начальная папка
             PrintInfo(dirIn, 0);
         }
 
-        static void PrintInfo(FileSystemInfo info, int a)
+        static void PrintInfo(FileSystemInfo info, int a)// метод для вывода остальных папок
         {
             string str = new string(' ', a);
             Console.WriteLine(str + info.Name);
 
-            if (info.GetType() == typeof(DirectoryInfo))
+            if (info.GetType() == typeof(DirectoryInfo))// выражение типа
             {
                 FileSystemInfo[] n = ((DirectoryInfo)info).GetFileSystemInfos();
                 for (int i = 0; i < n.Length; ++i)
                 {
-                    PrintInfo(n[i], a + 3);
+                    PrintInfo(n[i], a + 5);//расстояние
                 }
             }
         }
